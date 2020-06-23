81229e5b4daaa8a6c6e90f32b4ac1d32323c7bad
derrickip34 <derrickip34@gmail.com>
2020-06-23 14:07:39 +0300

    edit api views

--- main/urls.py
+++ main/urls.py
@@ -3,11 +3,7 @@ from . import views
 
 urlpatterns = [
     path('add_product-set/',views.AddProductSet.as_view(),name='add_set'),
-    path('delete_product-set/<pk>/',views.DeleteProductSet.as_view(),name='delete_set'),
-    path('update_product-set/<pk>/',views.UpdateProductSet.as_view(),name='update_set'),
-    path('all_product-sets/',views.AllProductSets.as_view(),name='view_all_set'),
-    path('get_product-set/<pk>/',views.GetProductSet.as_view(),name='get_set'),
-    path('update_products/<pk>/',views.UpdateProduct.as_view(),name='add_product'),
-    path('get_product/<pk>/',views.GetProduct.as_view(),name='get_product'),
-    path('all_products/',views.AllProducts.as_view())
+    path('product-set/details/<pk>/',views.ProductSetDetails.as_view(),name='update_set'),
+    path('add_products/',views.AddProduct.as_view(),name='add_product'),
+    path('product/details/<pk>/',views.ProductDetails.as_view(),name='get_product'),
 ]
--- main/views.py
+++ main/views.py
@@ -5,34 +5,19 @@ from rest_framework.permissions import AllowAny
 from .models import *
 
 # Create your views here
-class AddProductSet(generics.CreateAPIView):
+class AddProductSet(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ProductSetSerializer
     queryset = ProductSet.objects.all()
 
-class DeleteProductSet(generics.RetrieveUpdateDestroyAPIView):
+class ProductSetDetails(generics.ListCreateAPIView):
     serializer_class = ProductSetSerializer
     queryset = ProductSet.objects.all()
 
-class UpdateProductSet(generics.RetrieveUpdateAPIView):
-    serializer_class = ProductSetSerializer
-    queryset = ProductSet.objects.all()
 
-class AllProductSets(generics.ListCreateAPIView):
-    serializer_class = ProductSetSerializer
-    queryset = ProductSet.objects.all()
-
-class GetProductSet(generics.RetrieveAPIView):
-    serializer_class = ProductSetSerializer
-    queryset = ProductSet.objects.all()
-
-class UpdateProduct(generics.RetrieveUpdateAPIView):
+class ProductDetails(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
 
-class GetProduct(generics.RetrieveAPIView):
-    serializer_class = ProductSerializer
-    queryset = Product.objects.all()
-
-class AllProducts(generics.ListAPIView):
+class AddProduct(generics.ListCreateAPIView):
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
