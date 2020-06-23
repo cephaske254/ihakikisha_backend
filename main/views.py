302ee35fca2a07c9ff84ab1f4d8595c2ad55e27d
cephaske254 <cephaske254@gmail.com>
2020-06-23 15:55:51 +0300

    fixed permission classes error

--- main/views.py
+++ main/views.py
@@ -2,40 +2,41 @@ from django.shortcuts import render
 from rest_framework import generics
 from . import serializers
 from rest_framework.permissions import AllowAny,IsAuthenticated
-from .models import Distributor, User, Shop, Manufacturer, Farmer
+from .models import Distributor, User, Shop, Manufacturer, Farmer, Rating, Package
 # Create your views here.
 
 class DistributorProfile(generics.ListCreateAPIView):
     serializer_class = serializers.DistributorProfileSerializer
     queryset = Distributor.objects.all()
-    permission_classes = (IsAuthenticated)
+    permission_classes = (IsAuthenticated,)
 
 class DistributorProfileDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = serializers.DistributorProfileSerializer
     queryset = Distributor.objects.all()
-    permission_classes = (IsAuthenticated)
+    permission_classes = (IsAuthenticated,)
 
 
 
 class ManufacturerProfile(generics.ListCreateAPIView):
     serializer_class = serializers.ManufacturerProfileSerializer
     queryset = Manufacturer.objects.all()
+    permission_classes = (IsAuthenticated,)
 
 class ManufacturerProfileDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = serializers.ManufacturerProfileSerializer
     queryset = Manufacturer.objects.all()
-    permission_classes = (IsAuthenticated)
+    permission_classes = (IsAuthenticated,)
 
 
 class FarmerProfile(generics.ListCreateAPIView):
     serializer_class = serializers.FarmerProfileSerializer
     queryset = Farmer.objects.all()
-    permission_classes = (IsAuthenticated)
+    permission_classes = (IsAuthenticated,)
 
 class FarmerProfileDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = serializers.FarmerProfileSerializer
     queryset = Farmer.objects.all()
-    permission_classes = (IsAuthenticated)
+    permission_classes = (IsAuthenticated,)
 
     
 class ShopProfile(generics.ListCreateAPIView):
