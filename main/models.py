c07ee6c181f1620c4bc4d9d31045f46040f002de
cephaske254 <cephaske254@gmail.com>
2020-06-26 10:04:12 +0300

    heroku

--- main/models.py
+++ main/models.py
@@ -167,7 +167,7 @@ def generate_qr(sender, instance, **kwargs):
 
     image_url = cloudinary_image
     instance.qr_code = image_url
-    os.remove(image_name)
+    os.remove(image_name) 
 
 
 @receiver(post_save)
