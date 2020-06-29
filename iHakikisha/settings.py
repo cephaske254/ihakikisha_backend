0544de24e1c4c4c757e4362502cfdad73c9b13e8
cephaske254 <cephaske254@gmail.com>
2020-06-29 14:01:28 +0300

    added id to retrieve response serializer

--- iHakikisha/settings.py
+++ iHakikisha/settings.py
@@ -54,7 +54,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES':[
         'rest_framework.permissions.IsAuthenticated'
-    ]
+    ],
 }
 
 MIDDLEWARE = [
--- main/models.py
+++ main/models.py
@@ -17,7 +17,7 @@ from cloudinary.models import CloudinaryField
 
 
 class BaseModel(models.Model):
-    uuid = models.UUIDField(default=uuid.uuid4, unique=True,)
+    uuid = models.UUIDField(default=uuid.uuid4, unique=True,editable=False)
 
     class Meta:
         abstract = True
